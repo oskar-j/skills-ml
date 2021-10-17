606901332af17dffd98b8340c7adb2af839027c7
ojarczyk <ojarczyk@mojorank.com>
2021-10-17 16:19:18 +0200

    version up

--- setup.py
+++ setup.py
@@ -8,21 +8,9 @@ with open('README.md') as readme_file:
 with open('requirements.txt') as requirements_file:
     requirements = requirements_file.readlines()
 
-with open('requirements_dev.txt') as requirements_file:
-    test_requirements = [
-        req for req in requirements_file.readlines()
-        if 'git+' not in req
-    ] + requirements
-
-with open('requirements_addon.txt') as requirements_file:
-    addon_requirements = requirements_file.readlines()
-
-with open('requirements_viz.txt') as requirements_file:
-    viz_requirements = requirements_file.readlines()
-
 setup(
     name='Skills ML',
-    version='2.1.0',
+    version='2.1.1',
     description='Algorithms for Jobs/skills taxonomy creation',
     author="Center for Data Science and Public Policy",
     author_email='datascifellows@gmail.com',
@@ -30,10 +18,10 @@ setup(
     packages=find_packages(include=['skills_ml*']),
     include_package_data=True,
     install_requires=requirements,
-    extras_require={
-        'tensorflow': addon_requirements,
-        'viz': viz_requirements,
-    },
+    # extras_require={
+    #     'tensorflow': addon_requirements,
+    #     'viz': viz_requirements,
+    # },
     license="MIT license",
     keywords='nlp jobs skills onet',
     classifiers=[
@@ -42,8 +30,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     test_suite='tests',
-    tests_require=test_requirements
+    # tests_require=test_requirements
 )
